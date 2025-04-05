3528534ed0b6044e9e2f04744ed472d54dc91726
Roma Bludilin <romabludilin@yandex.ru>
2025-04-06 03:13:14 +0400

    fix: чиним актионс

--- backend/src/database/database.module.ts
+++ backend/src/database/database.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { DataSourceOptions } from 'typeorm'; // Добавляем импорт
 import { Film } from 'src/films/entities/film.entity';
 import { Schedule } from 'src/films/entities/schedule.entity';
 
@@ -9,20 +10,20 @@ import { Schedule } from 'src/films/entities/schedule.entity';
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
-      useFactory: (config: ConfigService) => {
-        const driver = config.get<'postgres'| 'sqlite'>('DATABASE_DRIVER', 'postgres');
-        return{
-        type: driver, 
-        host: config.get('DATABASE_HOST'),
-        port: config.get('DATABASE_PORT'),
-        username: config.get('DATABASE_USERNAME'),
-        password: config.get('DATABASE_PASSWORD'),
-        database: config.get('DATABASE_NAME'),
-        entities: [Film,Schedule],
-        synchronize: false,
-        // logging: true, 
-        // logger: 'advanced-console',
-      }},
+      useFactory: (config: ConfigService): DataSourceOptions => { // Явно указываем тип
+        const driver = config.get<'postgres' | 'sqlite'>('DATABASE_DRIVER', 'postgres');
+        
+        return {
+          type: driver,
+          host: config.get('DATABASE_HOST'),
+          port: config.get<number>('DATABASE_PORT'), // Явно указываем тип для порта
+          username: config.get('DATABASE_USERNAME'),
+          password: config.get('DATABASE_PASSWORD'),
+          database: config.get('DATABASE_NAME'),
+          entities: [Film, Schedule],
+          synchronize: false,
+        };
+      },
     }),
   ],
   exports: [TypeOrmModule],
